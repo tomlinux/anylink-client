bb65034a18a84d93b4a5b47b0bd802303fa96050
xuliang <xuliang12187@gmail.com>
2025-07-26 12:47:04 +0800

    fix-7

--- src/profilemanager.cpp
+++ src/profilemanager.cpp
@@ -228,3 +228,13 @@ QString ProfileManager::getOTPSecret() const
 {
     return opt_secret;
 }
+
+void ProfileManager::setOTPSecret(const QString &secret)
+{
+    opt_secret = secret;
+}
+
+QString ProfileManager::getOTPSecret() const
+{
+    return opt_secret;
+}
